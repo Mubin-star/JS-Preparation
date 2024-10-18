52c7a1a2fed4e3c7b23b144d4cbf37fbbf526e6b
Md.Mubinur Rahman <mubinurr274@gmail.com>
2024-10-18 19:41:17 +0000

    variable

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmail = "mubinurr274@gmail.com"
+var accountPassword = "12345"
+accountCity = "Dhaka"
+let accountState;
+// accountId = 2 // not allowes
+
+
+accountEmail = "ramika@gmail.com"
+accountPassword = "7778787"
+accountCity = "lalbagh"
+
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+
+
+
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
