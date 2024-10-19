84a4f1dee0b9abb14e50ea23843fd1dc8819870a
Md.Mubinur Rahman <mubinurr274@gmail.com>
2024-10-19 14:36:45 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" => 33
+//"33bc" => Nan
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggeIn = Boolean(isLoggeIn)
+console.log();
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
