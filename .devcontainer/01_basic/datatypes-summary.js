ccdb36011caf4acac917b1285af04d1a6300edc5
Md.Mubinur Rahman <mubinurr274@gmail.com>
2024-10-19 19:32:54 +0000

    datatype-summary

---
+++ .devcontainer/01_basic/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3534564596459743657n
+
+
+// Reference (Non Primitive)
+
+// Array, Objects,Functions
+
+
+const heros = ["superman", "batman", "Hunk"];
+let myObj = {
+   name: "Mubin",
+   age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction);
