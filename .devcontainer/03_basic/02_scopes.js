3dca4e71c0ccd0c8e883ce206792480cd8cef7d9
Md.Mubinur Rahman <mubinurr274@gmail.com>
2024-10-23 19:25:58 +0000

    scopes2

--- .devcontainer/03_basic/02_scopes.js
+++ .devcontainer/03_basic/02_scopes.js
@@ -15,6 +15,49 @@ for(let i = 0; i < array.length; i++){
 }
 
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+
+function one(){
+    const username = "Mubin"
+
+    function two(){
+     const website = "youtube"
+     console.log(username);
+    }
+    //console.log(website);
+
+    two()
+
+}
+
+//one()
+
+if (true) {
+    const username = "mubin"
+    if (username === "mubin"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+
+
+/// ++++++++++++++++++++ intersting ++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
