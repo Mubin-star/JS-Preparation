896491547a845aa9839155e334df82194618ab6a
Md.Mubinur Rahman <mubinurr274@gmail.com>
2024-10-23 18:24:21 +0000

    function2

--- .devcontainer/03_basic/01_functions.js
+++ .devcontainer/03_basic/01_functions.js
@@ -42,5 +42,36 @@ function loginUserMessage(username = "ramika"){
 }
 
 //console.log(loginUserMessage("Mubin"))
-console.log(loginUserMessage())
-   
+//console.log(loginUserMessage())
+
+function calculateCartPrice(val1, val2, ...num1){
+  return num1
+}
+
+//console.log(calculateCartPrice(200, 400, 500, 20000))
+
+const user = {
+   username: " mubin",
+   price: 199
+}
+
+function handleObject(anyobject){
+  console.log(`Username is ${anyobject.username} and price is $
+    {anyobject.price}`);
+}
+
+//handleObject(user)
+handleObject({
+  username: "ramika",
+  price: 499
+})
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+  return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
+
+
