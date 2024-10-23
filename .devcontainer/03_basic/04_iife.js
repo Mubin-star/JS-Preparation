e910b60410d249001d4f58b8411c8282a7d1a434
Md.Mubinur Rahman <mubinurr274@gmail.com>
2024-10-23 20:12:42 +0000

    iife

---
+++ .devcontainer/03_basic/04_iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(functin bro(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+(  (name)  => {
+    console.log(`DB CONNECTED TWO  ${name}`);
+}  )('mubin')
