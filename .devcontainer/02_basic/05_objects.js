64d36cfe619f6533e2b7dc6acd11f7766b788df6
Md.Mubinur Rahman <mubinurr274@gmail.com>
2024-10-23 16:02:09 +0000

    objects_final

---
+++ .devcontainer/02_basic/05_objects.js
@@ -0,0 +1,95 @@
+const course = {
+    coursename: "js in programming",
+    price: "999",
+    courseInstructor: "Mubin"
+}
+
+//course.courseInstructor
+
+
+const {courseInstructor: courseInstructor} = course
+//console.log(courseInstructor);
+
+
+//const navbar = ({company}) => {
+
+}
+
+//navbar(company = "Mubin");
+
+//{
+   // "name": "mubin",
+   // "coursename": "js in boss",
+   // "price": "free"
+//}
+[
+
+]
+//json api random api
+{
+    "results": [
+      {
+        "gender": "female",
+        "name": {
+          "title": "Miss",
+          "first": "Jennie",
+          "last": "Nichols"
+        },
+        "location": {
+          "street": {
+            "number": 8929,
+            "name": "Valwood Pkwy",
+          },
+          "city": "Billings",
+          "state": "Michigan",
+          "country": "United States",
+          "postcode": "63104",
+          "coordinates": {
+            "latitude": "-69.8246",
+            "longitude": "134.8719"
+          },
+          "timezone": {
+            "offset": "+9:30",
+            "description": "Adelaide, Darwin"
+          }
+        },
+        "email": "jennie.nichols@example.com",
+        "login": {
+          "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+          "username": "yellowpeacock117",
+          "password": "addison",
+          "salt": "sld1yGtd",
+          "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+          "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+          "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+        },
+        "dob": {
+          "date": "1992-03-08T15:13:16.688Z",
+          "age": 30
+        },
+        "registered": {
+          "date": "2007-07-09T05:51:59.390Z",
+          "age": 14
+        },
+        "phone": "(272) 790-0888",
+        "cell": "(489) 330-2385",
+        "id": {
+          "name": "SSN",
+          "value": "405-88-3636"
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/men/75.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+        },
+        "nat": "US"
+      }
+    ],
+    "info": {
+      "seed": "56d27f4a53bd5441",
+      "results": 1,
+      "page": 1,
+      "version": "1.4"
+    }
+  }
+  {"results":[{"gender":"female","name":{"title":"Ms","first":"Carla","last":"Vargas"},"location":{"street":{"number":2270,"name":"Avenida de La Albufera"},"city":"Orense","state":"Canarias","country":"Spain","postcode":41922,"coordinates":{"latitude":"-37.8046","longitude":"1.9249"},"timezone":{"offset":"+11:00","description":"Magadan, Solomon Islands, New Caledonia"}},"email":"carla.vargas@example.com","login":{"uuid":"2faf3293-5b0c-46bc-9538-afd69c8fc1d1","username":"lazymouse953","password":"camero1","salt":"SdVzUXfv","md5":"f687acc73b04eb2a3eb3a945ccef1198","sha1":"957f275767bfb1c72a449b874d983e7519bfd591","sha256":"8d01957a20772508895a02566ea9a581a77b194375caa5f371e8114aeb95498a"},"dob":{"date":"1992-02-22T17:12:02.316Z","age":32},"registered":{"date":"2010-08-06T09:11:47.660Z","age":14},"phone":"975-970-800","cell":"684-413-840","id":{"name":"DNI","value":"75190285-O"},"picture":{"large":"https://randomuser.me/api/portraits/women/57.jpg","medium":"https://randomuser.me/api/portraits/med/women/57.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/57.jpg"},"nat":"ES"}],"info":{"seed":"2f78eae1e20ec9cf","results":1,"page":1,"version":"1.4"}}
