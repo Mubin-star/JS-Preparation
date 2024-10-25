8a48e3f61476a2b82f6ce5ae1dae38f168f4c19a
Md.Mubinur Rahman <mubinurr274@gmail.com>
2024-10-25 19:29:20 +0000

    Four

---
+++ .devcontainer/05_iterations/Four.js
@@ -0,0 +1,28 @@
+
+
+const myObject = {
+    js: 'javaScript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject){
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+const map = new Map()
+map.set('USA',"United States of America")
+map.set('UK',"United Kingdom")
+map.set('Fr', "France")
+
+for (const key in map) {
+    console.log(key);
+}
